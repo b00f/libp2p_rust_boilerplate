713c9a7fe7c16b1b20d20ccc79e326f51b1a4bc8
Mostafa <mostafa.sedaghat@gmail.com>
2020-10-20 12:42:30 +0800

    No sleep after running the node

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,6 @@ use network::Network;
 use simple_logger::SimpleLogger;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
-use std::{thread, time::Duration};
 
 #[tokio::main]
 async fn main() {
@@ -20,17 +19,15 @@ async fn main() {
     let conf = Config::default();
     let network = Network::new(conf).unwrap();
 
+    let self_id = network.peer_id();
     let message_sender = network.message_sender();
     let message_receiver = network.message_receiver();
     let event_receiver = network.event_receiver();
 
+    println!("Our id is {}", self_id);
     let network_task = tokio::task::spawn(async {
         network.run().await;
     });
-
-    // TODO: WHY??
-    let delay = Duration::from_millis(2000);
-    thread::sleep(delay);
 
     tokio::task::spawn(async move {
         loop {
@@ -45,11 +42,11 @@ async fn main() {
             if let Ok(event) = event_receiver.recv().await {
                 match event {
                     network::Event::PeerConnected(peer_id) => {
-                        let msg = Message::Greeting(format!("Hello {}", peer_id));
+                        let msg = Message::Greeting(format!("Hello {} from {}", peer_id, self_id));
                         message_sender.send(msg).await;
                     }
                     network::Event::PeerDisconnected(peer_id) => {
-                        let msg = Message::Greeting(format!("Goodbye {}", peer_id));
+                        let msg = Message::Greeting(format!("Goodbye {} from {}", peer_id, self_id));
                         message_sender.send(msg).await;
                     }
                 }
